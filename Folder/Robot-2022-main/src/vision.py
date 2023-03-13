a9916f21bcac945a005e19fb6e4deb47a578f591
Keagan06 <97635506+Keagan06@users.noreply.github.com>
2023-03-13 09:58:00 -0400

    idk what this is

--- Folder/Robot-2022-main/src/vision.py
+++ Folder/Robot-2022-main/src/vision.py
@@ -6,5 +6,18 @@
 import wpilib
 
 
+
 def cameraLaunch():
+    
     wpilib.CameraServer.launch()
+
+#from cscore import CameraServer
+
+#def main():
+    #cs = CameraServer.getInstance()
+    #cs.enableLogging()
+
+    #usb1 = cs.startAutomaticCapture(dev=0)
+    #usb2 = cs.startAutomaticCapture(dev=1)
+
+    #cs.waitForever()
